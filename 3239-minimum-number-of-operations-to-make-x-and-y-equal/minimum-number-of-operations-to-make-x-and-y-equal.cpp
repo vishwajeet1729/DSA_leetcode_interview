597ac4802ab763f441518bbe1f4f49e3409e0b96
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-03-08 01:58:32 +0530

    Time: 14 ms (59.81%) | Memory: 56.2 MB (15.66%) - LeetSync

---
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    
+public:
+    int solve(int x, int y, vector<int> &dp){
+        
+    if(x <= y) return y - x;
+        
+    if(dp[x] != -1) return dp[x];
+     
+    //case 1
+    int res = abs(x - y);                                 
+    
+    //case 2
+    res = min(res, 1 + x%5 + solve(x/5, y, dp));              
+    
+    //case 3
+    res = min(res, 1 + (5 - x%5) + solve(x/5 + 1, y, dp));    
+    
+    //case 4
+    res = min(res, 1 + x%11 + solve(x/11, y, dp));            
+    
+    //case 5
+    res = min(res, 1 + (11 - x%11) + solve(x/11 + 1, y, dp)); 
+        
+    return dp[x] = res;
+}
+public:
+    int minimumOperationsToMakeEqual(int x, int y) {
+        
+        vector<int> dp(10001, -1);
+        return solve(x, y, dp);  
+    }
+};
