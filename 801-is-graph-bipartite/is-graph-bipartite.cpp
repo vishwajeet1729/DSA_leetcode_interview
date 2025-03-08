c4840926f2da4c21914499b9836ee6bf724254a7
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-03-08 14:37:29 +0530

    Time: 3 ms (16.67%) | Memory: 18.8 MB (8.47%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> s1, s2, vis;
+    vector<vector<int>> v;
+
+    void dfs(int node, bool turn) {
+        if (vis[node]) return;
+        
+        if (turn) s1.push_back(node);
+        else s2.push_back(node);
+        
+        vis[node] = 1;
+
+        for (int neighbor : v[node]) {
+            if (!vis[neighbor]) 
+                dfs(neighbor, !turn);
+        }
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        v = graph;
+        int n = v.size();
+        vis.assign(n, 0);
+        
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                s1.clear();
+                s2.clear();
+                dfs(i, true);
+                                for (int i = 0; i < s1.size(); ++i) {
+                    for (int j = i + 1; j < s1.size(); ++j) {
+                        for (int neighbor : v[s1[i]]) {
+                            if (neighbor == s1[j]) return false;
+                        }
+                    }
+                }
+                for (int i = 0; i < s2.size(); ++i) {
+                    for (int j = i + 1; j < s2.size(); ++j) {
+                        for (int neighbor : v[s2[i]]) {
+                            if (neighbor == s2[j]) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
