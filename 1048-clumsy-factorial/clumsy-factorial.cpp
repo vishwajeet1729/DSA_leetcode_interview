f9a4b17e7f18aa935a8bdda68f4d412a33c344c5
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-03-20 23:04:39 +0530

    Time: 101 ms (5.42%) | Memory: 10.5 MB (6.63%) - LeetSync

--- 1048-clumsy-factorial/clumsy-factorial.cpp
+++ 1048-clumsy-factorial/clumsy-factorial.cpp
@@ -1,20 +1,44 @@
 class Solution {
- public:
-  int clumsy(int N) {
-    if (N == 1)
-      return 1;
-    if (N == 2)
-      return 2;
-    if (N == 3)
-      return 6;
-    if (N == 4)
-      return 7;
-    if (N % 4 == 1)
-      return N + 2;
-    if (N % 4 == 2)
-      return N + 2;
-    if (N % 4 == 3)
-      return N - 1;
-    return N + 1;
-  }
+public:
+
+    int clumsy(int n) {
+        stack<int>st;
+        st.push(n);
+        int t = 1;
+        for(int i=n-1; i>0; i--){
+            int x = st.top();
+            if(t==1){
+                st.pop();
+                st.push(x*i);
+                t=2;
+                continue;
+            }
+            else if(t==2){
+                st.pop();
+                st.push(x/i);
+                t=3;
+                continue;
+
+            }
+            else if(t==3){
+                st.pop();
+                st.push(x+i);
+                t=4;
+                continue;
+            }
+            else if(t==4){
+                st.push(-1*i);
+                t=1;
+            }
+        }
+
+        int ans =0;
+        while(!st.empty()){
+            cout<<st.top()<<" ";
+            ans += st.top();
+            st.pop();
+        }
+        cout<<ans<<" ";
+        return ans;
+    }
 };
