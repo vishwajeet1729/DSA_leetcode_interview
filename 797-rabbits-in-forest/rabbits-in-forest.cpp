1f63ec22a0453c7a266cbf7bfc19162e233d33e8
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-04-20 20:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (91.60%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& v) {
+    int n=v.size();
+
+    unordered_map<int,int>m;
+
+      int ct=0;
+    for(int i=0;i<n;++i){
+          
+     m[v[i]+1]++;
+     if(m[v[i]+1]==v[i]+1){
+        ct+=v[i]+1;
+        m.erase(m[v[i]+1]);
+     }
+
+    }
+    for(auto it:m){
+                ct+=(it.first);
+    }
+    return ct;
+      
+    }
+};
