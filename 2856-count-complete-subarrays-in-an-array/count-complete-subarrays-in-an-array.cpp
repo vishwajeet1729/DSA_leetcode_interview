aa75bdfdc087667a303c49365cdbfe5a7ff196c8
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-04-24 11:53:17 +0530

    Time: 22 ms (54.35%) | Memory: 41.8 MB (59.44%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        //n^2logn eeasy solution 
+        // can we optmize it
+        //we can optmzie it using unorderes map like n^2
+        //can we again optmize it
+
+        int k = unordered_set<int>(nums.begin(),nums.end()).size();
+        unordered_map<int, int> mpp;
+       int left=0,right=0,res=0;
+        for (int i = 0; i < nums.size(); ++i) {
+            mpp[nums[i]]++;
+            while (mpp.size() == k) {
+                res += nums.size() - i;  
+                mpp[nums[left]]--;
+                if (mpp[nums[left]] == 0) mpp.erase(nums[left]);
+                left++;
+            }
+        }
+        return res;
+
+
+    }
+};
