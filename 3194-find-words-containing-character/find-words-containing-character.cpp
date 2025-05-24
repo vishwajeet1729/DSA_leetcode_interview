a2c3c550c06400e3112d1d8082fb699e4f9859d4
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-05-24 11:11:58 +0530

    Time: 5 ms (7.15%) | Memory: 33.1 MB (56.49%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& w, char x) {
-int ct=0;
-int n=w.size();
 vector<int>ans;
-         for(int i=0;i<n;++i){
-               if(w[i].find(x)!=string::npos)ans.push_back(i);
-
-         }
+         for(int i=0;i<w.size();++i)if(w[i].find(x)!=string::npos)ans.push_back(i);
          return ans;
 
     }  
