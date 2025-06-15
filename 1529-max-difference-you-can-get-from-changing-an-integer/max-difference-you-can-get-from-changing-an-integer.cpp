189459beb9f3845d99ee58cb401c876712993e0a
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-06-15 14:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (28.91%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxDiff(int n) {
+          
+ string s1=to_string(n);
+      string s2=to_string(n);
+     //min   check the first no which is not 1  store that digit in a
+     char a='@',b='@';int ind=-1;
+     for(int i=0;i<s1.size();++i){
+        if(s1[i]!='1'&&s1[i]!='0'){
+            ind=i;
+            a=s1[i];break;
+        }
+     }
+     for(int i=0;i<s1.size();++i){
+        if(s1[i]==a&&ind!=0)s1[i]='0';
+        else
+        if(s1[i]==a)s1[i]='1';}
+
+
+     // max check jo ki 9 nahi hai
+    
+     for(int i=0;i<s2.size();++i){
+        if(s2[i]!='9'){
+            b=s2[i];
+            break;
+        }
+     }
+     for(int i=0;i<s2.size();++i)if(s2[i]==b)s2[i]='9';
+    int x=stoi(s1),y=stoi(s2);
+    return abs(x-y); 
+    }
+};
