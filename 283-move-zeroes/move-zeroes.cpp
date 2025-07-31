b13650ab180ceb50f9955be21e42927de0f42875
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-07-31 13:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (80.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;  // Pointer to place the next non-zero element
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]!=0){
+                // Swap current element with the element at index j 
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            
+                j++;  // Move j to the next index for placing non-zero
+            }
+        }
+    }
+};
