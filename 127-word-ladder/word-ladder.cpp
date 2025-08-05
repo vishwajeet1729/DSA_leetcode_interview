8529ce3a37dd98b6caafa4622b6724dd06875aa3
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2025-08-05 19:42:32 +0530

    Time: 59 ms (62.53%) | Memory: 22 MB (28.77%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
- bool check(string s1,string s2){
-    int flag=0;
-      for(int i=0;i<s1.size();++i){
-        if(s1[i]!=s2[i])flag++;
-      }
-      return flag==1;
-}
-    int ladderLength(string beg, string end, vector<string>& v) {
-           queue<string>q;
-    q.push(beg);
-    int n=v.size();
-   vector<bool>vis(n,0);
-   int ct=0;
-     while(!q.empty()){
-          queue<string>demo;
-          ct++;
-          int x=q.size();
-          for(int i=0;i<x;++i)
-          {
-             string s=q.front();
-             q.pop();
-             for(int j=0;j<n;++j){
-            if(vis[j])continue;
-             if(check(s,v[j])){
-                if(v[j]==end){
-                return ct+1;
+    int ladderLength(string begin, string end, vector<string>& wor) {
+             int n=wor.size();
+             unordered_map<string,int>m;
+             for(int i=0;i<n;++i)m[wor[i]]++;
+
+             //bfs
+        queue<pair<string,int>>q;
+        q.push({begin,1});
+
+        while(!q.empty())
+        {
+                string s=q.front().first;
+               
+                 int cost=q.front().second;
+                  if(s==end)return cost;
+                q.pop();
+
+                int len=s.size();
+               
+                for(int i=0;i<len;++i){
+                   char c=s[i];        
+                        for(int j=0;j<26;++j){
+                           s[i]=(char)((int)'a'+j); 
+
+                           if(m.count(s)){
+                             q.push({s,cost+1});
+                             m.erase(s);
+                           }
+
+                        }
+                        s[i]=c;
+
                 }
-                // demo.push(v[j]);
-                q.push(v[j]);
-                vis[j]=1;}  
-             }
-          }
-     }
-    //  cout<<0<<endl;
-    return 0;
+                 
+
+        }
+        return 0;
+
 
     }
 };
