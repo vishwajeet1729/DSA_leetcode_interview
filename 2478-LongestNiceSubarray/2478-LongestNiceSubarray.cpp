4f1fd8ba0661820fe31564f3b796c69a41f8a835
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:29:02 +0530

    Create 2478-LongestNiceSubarray

---
+++ 2478-LongestNiceSubarray/2478-LongestNiceSubarray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/17/2026, 12:29:02 PM
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int l = 0, usedBits = 0, maxLength = 0;
+
+        for (int r = 0; r < nums.size(); r++) {
+            while ((usedBits & nums[r]) != 0) {
+                usedBits ^= nums[l];
+                l++;
+            }
+
+            usedBits |= nums[r];
+            maxLength = max(maxLength, r - l + 1);
+        }
+
+        return maxLength;
+    }
+};
