71459b9e22e9cac351d9b56d8eff7c00a0d5ce76
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:27:28 +0530

    Create 3383-TakingMaximumEnergyFromTheMysticDungeon

---
+++ 3383-TakingMaximumEnergyFromTheMysticDungeon/3383-TakingMaximumEnergyFromTheMysticDungeon.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/17/2026, 12:27:27 PM
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size(), ans = INT_MIN;
+        for (int i = n - k; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j >= 0; j -= k) {
+                sum += energy[j];
+                ans = max(ans, sum);
+            }
+        }
+        return ans;
+    }
+};
