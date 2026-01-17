a95f734e4562817d51df7385ca7bc666ef9f2aa8
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:31:04 +0530

    Create 1387-FindElementsInAContaminatedBinaryTree

---
+++ 1387-FindElementsInAContaminatedBinaryTree/1387-FindElementsInAContaminatedBinaryTree.cpp
@@ -0,0 +1,18 @@
+// Last updated: 1/17/2026, 12:31:04 PM
+class FindElements {
+public:
+    FindElements(TreeNode* root) { dfs(root, 0); }
+
+    bool find(int target) { return seen.count(target) > 0; }
+
+private:
+    unordered_set<int> seen;
+
+    void dfs(TreeNode* currentNode, int currentValue) {
+        if (!currentNode) return;
+        // visit current node by adding its value to seen
+        seen.insert(currentValue);
+        dfs(currentNode->left, currentValue * 2 + 1);
+        dfs(currentNode->right, currentValue * 2 + 2);
+    }
+};
