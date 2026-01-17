fe41b5cc9cc7ab647358b925f098f943b2be189d
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:26:49 +0530

    Create 3519-FindTheNumberOfWinningPlayers

---
+++ 3519-FindTheNumberOfWinningPlayers/3519-FindTheNumberOfWinningPlayers.cpp
@@ -0,0 +1,26 @@
+// Last updated: 1/17/2026, 12:26:49 PM
+class Solution {
+public:
+    int winningPlayerCount(int n, vector<vector<int>>& pick) {
+        vector<unordered_map<int, int>> playerPicks(n);
+    
+    for (const auto& p : pick) {
+        int player = p[0];
+        int color = p[1];
+        playerPicks[player][color]++;
+    }
+    
+    int winnersCount = 0;
+    for (int i = 0; i < n; ++i) {
+        for (const auto& entry : playerPicks[i]) {
+            if (entry.second > i) {
+                winnersCount++;
+                break;
+            }
+        }
+    }
+    
+    return winnersCount;
+      
+    }
+};
