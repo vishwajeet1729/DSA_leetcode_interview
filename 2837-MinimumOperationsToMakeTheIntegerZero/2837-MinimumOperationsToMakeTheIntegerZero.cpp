92d99f4f73631c06b9d9a1c60bf1d10013c6bfac
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:28:28 +0530

    Create 2837-MinimumOperationsToMakeTheIntegerZero

---
+++ 2837-MinimumOperationsToMakeTheIntegerZero/2837-MinimumOperationsToMakeTheIntegerZero.cpp
@@ -0,0 +1,17 @@
+// Last updated: 1/17/2026, 12:28:28 PM
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        int k = 1;
+        while (true) {
+            long long x = num1 - static_cast<long long>(num2) * k;
+            if (x < k) {
+                return -1;
+            }
+            if (k >= __builtin_popcountll(x)) {
+                return k;
+            }
+            k++;
+        }
+    }
+};
