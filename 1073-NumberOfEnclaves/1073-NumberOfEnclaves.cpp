79c23edd07c7d6679a71360ea7b3b7aa19aa9642
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:31:29 +0530

    Create 1073-NumberOfEnclaves

---
+++ 1073-NumberOfEnclaves/1073-NumberOfEnclaves.cpp
@@ -0,0 +1,39 @@
+// Last updated: 1/17/2026, 12:31:29 PM
+class Solution {
+public:
+int n,m;
+
+void rec(int i,int j,vector<vector<int>>&v){
+       if(i>=n||j>=m||i<0||j<0||v[i][j]==0)return;
+       v[i][j]=0;
+       rec(i+1,j,v);
+       rec(i,j+1,v);
+       rec(i-1,j,v);
+       rec(i,j-1,v);
+  
+}
+    int numEnclaves(vector<vector<int>>& v) {
+
+     n=v.size(),m=v[0].size();
+    // vector<vector<char>>vis(n,vector<char>(m,0));
+     
+    for(int i=0;i<n;++i){
+            if(v[i][0]==1)rec(i,0,v);
+            if(v[i][m-1]==1)rec(i,m-1,v);
+    }
+    for(int i=0;i<m;++i){
+        if(v[0][i]==1)rec(0,i,v);
+        if(v[n-1][i]==1)rec(n-1,i,v);
+    }
+    int ct=0;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+          if(v[i][j])ct++;
+        }
+        // line;
+    }
+
+// cout<<ct<<endl;
+return ct;
+    }
+};
