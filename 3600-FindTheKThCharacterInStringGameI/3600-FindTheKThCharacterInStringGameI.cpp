cac6a20c3a7a2844edeb6693fd85672f48698e58
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:26:37 +0530

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 1/17/2026, 12:26:37 PM
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+
+    while (word.length() < k) {
+        string nextPart = "";
+        for (char c : word) {
+            // Shift to next character, 'z' wraps to 'a'
+            char nextChar = (c - 'a' + 1) % 26 + 'a';
+            nextPart += nextChar;
+        }
+        word += nextPart;
+    }
+
+    return word[k - 1];
+    }
+};
