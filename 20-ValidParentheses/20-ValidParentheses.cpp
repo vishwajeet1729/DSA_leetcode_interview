cf95ac36549475b7a7ccdf0966c8894298e13ce6
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:35:15 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/17/2026, 12:35:14 PM
+class Solution
+{
+public:
+   bool isValid(string s) {
+    vector<char> v;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            v.push_back(s[i]);
+        else {
+            if (v.empty() ||
+                (s[i] == ')' && v.back() != '(') ||
+                (s[i] == ']' && v.back() != '[') ||
+                (s[i] == '}' && v.back() != '{'))
+                return false;
+            else
+                v.pop_back();
+        }
+    }
+
+    return v.empty(); 
+}
+
+};
