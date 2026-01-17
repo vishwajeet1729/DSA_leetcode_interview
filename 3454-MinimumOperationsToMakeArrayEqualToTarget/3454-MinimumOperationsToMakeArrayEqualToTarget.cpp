4f8a023aa11ab6260ffe9981bb75541cc12b44ec
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:27:06 +0530

    Create 3454-MinimumOperationsToMakeArrayEqualToTarget

---
+++ 3454-MinimumOperationsToMakeArrayEqualToTarget/3454-MinimumOperationsToMakeArrayEqualToTarget.cpp
@@ -0,0 +1,31 @@
+// Last updated: 1/17/2026, 12:27:06 PM
+class Solution {
+public:
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+            
+           int n = nums.size();
+        long incr = 0, decr = 0, ops = 0;
+
+        for (int i = 0; i < n; i++) {
+            long diff = target[i] - nums[i];
+
+            if (diff > 0) {
+                if (incr < diff)
+                    ops += diff - incr;
+                incr = diff;
+                decr = 0;
+            } else if (diff < 0) {
+                if (decr < -diff)
+                    ops += -diff - decr;
+                decr = -diff;
+                incr = 0;
+            } else {
+                incr = decr = 0;
+            }
+        }
+
+        return ops;
+
+
+    }
+};
