d78d0188fc0f3ce3ebdb1ec5982b5cd955f45b80
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:32:16 +0530

    Create 717-1BitAnd2BitCharacters

---
+++ 717-1BitAnd2BitCharacters/717-1BitAnd2BitCharacters.cpp
@@ -0,0 +1,11 @@
+// Last updated: 1/17/2026, 12:32:16 PM
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int i = 0;
+        while (i < bits.size() - 1) {
+            i += bits[i] + 1;
+        }
+        return i == bits.size() - 1;
+    }
+};
