1470d49ef6d5f6f89f02a56ddb6ab3379f1bb1fc
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:34:13 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,35 @@
+// Last updated: 1/17/2026, 12:34:13 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+         queue<TreeNode *>q;
+         int ct=0;
+         if(!root)return 0;
+         q.push(root);
+         ct++;
+         while(!q.empty()){
+              int n=q.size();
+              for(int i=0;i<n;++i){
+                   TreeNode *x=q.front();
+                   q.pop();
+                   if(!x->left&&!x->right)return ct;
+                   if(x->right)q.push(x->right);
+                   if(x->left)q.push(x->left);
+                
+              }
+              ct++;
+         }
+         return ct;
+    }
+};
