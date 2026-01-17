6f46a277b675dc4e814ae908febdaee49b2fbc1c
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:34:36 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,38 @@
+// Last updated: 1/17/2026, 12:34:36 PM
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& v) {
+         int n=v.size(),m=v[0].size();
+        bool row=false,col=0;
+        if(v[0][0]==0){row=1,col=1;}
+        for(int i=0;i<m;++i)if(v[0][i]==0)row=1;
+        for(int i=0;i<n;++i)if(v[i][0]==0)col=1;
+       for(int i=1;i<m;++i){
+          if(v[0][i]==0)continue;
+          for(int j=1;j<n;++j){
+             if(v[j][i]==0){
+                v[0][i]=0;
+                break;
+             }
+          }
+       }
+        for(int i=1;i<n;++i){
+              
+              if(v[i][0]==0)continue;
+              for(int j=1;j<m;++j){
+                   if(v[i][j]==0){v[i][0]=0;break;}
+              }
+         }  
+
+          for(int i=1;i<m;++i){
+              
+              if(v[0][i]==0)for(int j=0;j<n;++j)v[j][i]=0;
+          }
+
+          for(int i=1;i<n;++i){
+               if(v[i][0]==0)for(int j=0;j<m;++j)v[i][j]=0;
+          }
+           if(row)for(int i=0;i<m;++i)v[0][i]=0;
+           if(col)for(int i=0;i<n;++i)v[i][0]=0;
+    }
+};
