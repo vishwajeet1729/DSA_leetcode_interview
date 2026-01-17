bc265d2471d48b2eac47921435118198623329cd
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:32:36 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -0,0 +1,23 @@
+// Last updated: 1/17/2026, 12:32:36 PM
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+       map<string,int>m;
+
+          function<int(int,int)>rec=[&](int x,int y)->int{
+             if(x>y)return 0;
+             if(x==y)return 1;       
+      string key=to_string(x)+"|"+to_string(y);
+       if(m.find(key)!=m.end())return m[key];
+             int ans=0;       
+             if(s[x]==s[y]) ans= 2+rec(x+1,y-1);
+             
+             else ans=max(rec(x+1,y),rec(x,y-1));
+             return m[key]=ans;
+
+          };
+
+        //   cout<<rec(0,s.size()-1)<<endl;
+        return rec(0,s.size()-1);    
+    }
+};
