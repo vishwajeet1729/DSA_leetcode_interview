f1ab24016e6db46457c2e708f77c3d41ba5d087e
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:31:13 +0530

    Create 1264-MaximumNumberOfWordsYouCanType

---
+++ 1264-MaximumNumberOfWordsYouCanType/1264-MaximumNumberOfWordsYouCanType.cpp
@@ -0,0 +1,21 @@
+// Last updated: 1/17/2026, 12:31:13 PM
+class Solution {
+public:
+    int canBeTypedWords(string text, string broken) {
+        int mask = 0;
+        for (int i = 0; i < broken.size(); i++)
+            mask |= 1 << (broken[i] - 97);
+        
+        int count = 0;
+        int wordMask = 0;
+        for (int i = 0; i <= text.size(); i++) {
+            if (i < text.size() && text[i] >= 'a' && text[i] <= 'z')
+                wordMask |= mask & (1 << (text[i] - 97));
+            if (i == text.size() || text[i] == ' ') {
+                if (wordMask == 0) count++;
+                wordMask = 0;
+            }
+        }
+        return count;
+    }
+};
