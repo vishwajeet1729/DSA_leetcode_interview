23a205fe713e3a7654091f1c7160da412bcaaa4e
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:29:23 +0530

    Create 2308-DivideArrayIntoEqualPairs

---
+++ 2308-DivideArrayIntoEqualPairs/2308-DivideArrayIntoEqualPairs.cpp
@@ -0,0 +1,11 @@
+// Last updated: 1/17/2026, 12:29:23 PM
+class Solution {
+public:
+    bool divideArray(vector<int>& v) {
+         map<int,int>m;
+         for(int i=0;i<v.size();++i)m[v[i]]++;
+
+         for(auto it:m)if(it.second%2)return 0;
+         return 1;
+    }
+};
