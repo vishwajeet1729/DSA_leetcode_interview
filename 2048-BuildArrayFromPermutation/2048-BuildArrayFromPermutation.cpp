e9660ac11c08b57a9985712e5201c5a78cba55aa
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:30:03 +0530

    Create 2048-BuildArrayFromPermutation

---
+++ 2048-BuildArrayFromPermutation/2048-BuildArrayFromPermutation.cpp
@@ -0,0 +1,16 @@
+// Last updated: 1/17/2026, 12:30:03 PM
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        // Build the final value on the first iteration
+        for (int i = 0; i < n; ++i) {
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        }
+        // Modified to final value on the second iteration
+        for (int i = 0; i < n; ++i) {
+            nums[i] /= 1000;
+        }
+        return nums;
+    }
+};
