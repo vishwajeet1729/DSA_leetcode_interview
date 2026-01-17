ca0b4b0e97e63273b9e749fcc6be9e5e337ee9e4
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:33:40 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.cpp
@@ -0,0 +1,15 @@
+// Last updated: 1/17/2026, 12:33:40 PM
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for (int i = 0; i < 32; i++) {
+            ans =  ans<<1;
+            if(n&1){
+                ans=ans|1;
+            }
+            n = n>>1;
+        }
+        return ans;
+    }
+};
