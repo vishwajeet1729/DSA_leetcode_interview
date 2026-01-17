d99ee8d7ab5937f70d3280e59b6b419574bee2e6
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:25:27 +0530

    Create 3872-FindMostFrequentVowelAndConsonant

---
+++ 3872-FindMostFrequentVowelAndConsonant/3872-FindMostFrequentVowelAndConsonant.cpp
@@ -0,0 +1,22 @@
+// Last updated: 1/17/2026, 12:25:26 PM
+class Solution {
+public:
+    bool is_vowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    int maxFreqSum(string s) {
+        unordered_map<char, int> mp;
+        for (auto ch : s) {
+            mp[ch]++;
+        }
+        int vowel = 0, consonant = 0;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (is_vowel(ch)) {
+                vowel = max(vowel, mp[ch]);
+            } else {
+                consonant = max(consonant, mp[ch]);
+            }
+        }
+        return vowel + consonant;
+    }
+};
