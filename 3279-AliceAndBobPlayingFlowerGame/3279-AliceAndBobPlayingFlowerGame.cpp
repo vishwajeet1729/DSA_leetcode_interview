c62aa2c30fd568c1628e85d75112d73f0c9099da
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:27:52 +0530

    Create 3279-AliceAndBobPlayingFlowerGame

---
+++ 3279-AliceAndBobPlayingFlowerGame/3279-AliceAndBobPlayingFlowerGame.cpp
@@ -0,0 +1,9 @@
+// Last updated: 1/17/2026, 12:27:52 PM
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+          
+          return (long long)(n+1LL)/2LL*(m/2)+(m+1LL)/2*(n/2);
+
+    }
+};
