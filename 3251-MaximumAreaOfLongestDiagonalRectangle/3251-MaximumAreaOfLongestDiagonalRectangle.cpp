39fe711741f77fe17101ecf0606bd234c5d3d5ac
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:27:58 +0530

    Create 3251-MaximumAreaOfLongestDiagonalRectangle

---
+++ 3251-MaximumAreaOfLongestDiagonalRectangle/3251-MaximumAreaOfLongestDiagonalRectangle.cpp
@@ -0,0 +1,18 @@
+// Last updated: 1/17/2026, 12:27:57 PM
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        int maxDiaSq = 0, maxArea = 0;
+        for (const auto& dim : dimensions) {
+            int l = dim[0], w = dim[1];
+            int diaSq = l * l + w * w, area = l * w;
+            if (diaSq > maxDiaSq) {
+                maxDiaSq = diaSq;
+                maxArea = area;
+            } else if (diaSq == maxDiaSq) {
+                maxArea = max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+};
