f7dd616d30239a8a06eadc242c9eb40a0a60d58b
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:31:11 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.cpp
@@ -0,0 +1,16 @@
+// Last updated: 1/17/2026, 12:31:11 PM
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        // Loop through the array up to the third-to-last element
+        for (int i = 0; i < n - 2; i++) {
+            // Check if the current element and the next two elements are all
+            // odd
+            if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
