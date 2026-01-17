103fba9b7cb3fed75cae9333c054b8835d5b89c7
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:30:38 +0530

    Create 1604-LeastNumberOfUniqueIntegersAfterKRemovals

---
+++ 1604-LeastNumberOfUniqueIntegersAfterKRemovals/1604-LeastNumberOfUniqueIntegersAfterKRemovals.cpp
@@ -0,0 +1,29 @@
+// Last updated: 1/17/2026, 12:30:38 PM
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(std::vector<int>& v, int k) {
+        std::map<int, int> m;
+        int n = v.size();
+        for (int i = 0; i < n; i++)
+            m[v[i]]++;
+
+        std::vector<std::pair<int, int>> re;
+        for (auto it = m.begin(); it != m.end(); it++)
+            re.push_back({it->second, it->first});
+
+        std::sort(re.begin(), re.end());
+
+        int x = 0;
+        for (int i = 0; i < re.size(); i++) {
+            int p = re[i].first;
+            if (p <= k) {
+                k -= p;
+                x++;
+            } else {
+                break;
+            }
+        }
+
+        return re.size() - x;
+    }
+};
