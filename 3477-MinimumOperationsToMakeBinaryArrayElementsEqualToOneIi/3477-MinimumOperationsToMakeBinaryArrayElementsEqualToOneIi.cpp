3dd74129ca4833a4e62e7818ed2ba88f46f94a7b
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:27:00 +0530

    Create 3477-MinimumOperationsToMakeBinaryArrayElementsEqualToOneIi

---
+++ 3477-MinimumOperationsToMakeBinaryArrayElementsEqualToOneIi/3477-MinimumOperationsToMakeBinaryArrayElementsEqualToOneIi.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/17/2026, 12:27:00 PM
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+         int n = nums.size();
+        int operations = 0;
+        bool flip=false;
+        for (int i = 0; i <n; ++i) {
+            if(flip)nums[i]=1-nums[i];
+            if (nums[i] == 0) {
+                nums[i] = 1;
+                flip=!flip;
+                operations += 1;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                return -1;
+            }
+        }
+
+        return operations;
+    }
+};
