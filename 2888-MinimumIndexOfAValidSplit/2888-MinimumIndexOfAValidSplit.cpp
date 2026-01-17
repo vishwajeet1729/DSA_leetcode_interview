464ff4bf1fa79c97a7dc2b9b11796b942896822f
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:28:23 +0530

    Create 2888-MinimumIndexOfAValidSplit

---
+++ 2888-MinimumIndexOfAValidSplit/2888-MinimumIndexOfAValidSplit.cpp
@@ -0,0 +1,24 @@
+// Last updated: 1/17/2026, 12:28:23 PM
+class Solution {
+public:
+    int minimumIndex(vector<int>& v) {
+           int n=v.size();
+    map<int,int>left,right;
+
+    for(int i=0;i<n;++i)right[v[i]]++;
+int ans=-1;
+    for(int i=0;i<n;++i){
+        left[v[i]]++;
+        right[v[i]]--;
+        int leftSize=i+1;
+        int rightSize=n-i-1;
+        if(left[v[i]]>leftSize/2&&right[v[i]]>rightSize/2){
+            ans=i;
+            break;
+        }
+        
+    }
+    // cout<<ans<<endl;
+return ans;
+    }
+};
