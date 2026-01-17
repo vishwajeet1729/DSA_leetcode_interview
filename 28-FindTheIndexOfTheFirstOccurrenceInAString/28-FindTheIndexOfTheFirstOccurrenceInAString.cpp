cb771ed88d1d6f9db7837bf1d15f148abe7ca196
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:35:11 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,13 @@
+// Last updated: 1/17/2026, 12:35:11 PM
+class Solution {
+public:
+    int strStr(string a, string b) {
+           
+            if(a.find(b)==string::npos)return -1;
+
+             return a.find(b);
+
+
+    } 
+    
+};
