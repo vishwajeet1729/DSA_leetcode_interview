aef03b9c96d9d02760a50d5d5615f804ac5109f2
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:35:27 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/17/2026, 12:35:27 PM
+class Solution {
+public:
+   int reverse(int x) {
+   long  long result = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x = x / 10;
+
+        if (result > INT_MAX|| result < INT_MIN) {
+            return 0; // Overflow detected
+        }
+
+       
+
+        result = result * 10 + digit;
+    }
+   if (result > INT_MAX || result < INT_MIN) {
+        return 0; // Overflow detected after reversal
+    }
+    return (int)result;
+}
+
+};
