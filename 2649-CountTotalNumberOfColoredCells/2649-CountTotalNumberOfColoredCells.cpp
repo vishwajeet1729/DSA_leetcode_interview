bb6f6c58f87eeda9eabf931aa3de8365ab839132
Vishwajeet <124507172+vishwajeet1729@users.noreply.github.com>
2026-01-17 12:28:41 +0530

    Create 2649-CountTotalNumberOfColoredCells

---
+++ 2649-CountTotalNumberOfColoredCells/2649-CountTotalNumberOfColoredCells.cpp
@@ -0,0 +1,13 @@
+// Last updated: 1/17/2026, 12:28:40 PM
+class Solution {
+public:
+    long long coloredCells(int n) {
+         long long back=1;
+      for(int i=2;i<=n;++i){
+             if(i==n)return back+(i-2)*4+4;
+             back=back+(i-2)*4+4;
+             
+      } 
+    return 1;
+    }
+};
